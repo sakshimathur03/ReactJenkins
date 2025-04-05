4d83b2a6b9e0da389ad13f7b4d68d6a29a1a4cad
Sakshi Mathur <147843432+sakshimathur03@users.noreply.github.com>
2025-04-05 15:58:30 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -5,29 +5,30 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "rgasp" {
   location = var.location
-  name = var.resource_group_name
-  
+  name     = var.resource_group_name
 }
+
 data "azurerm_client_config" "name" {}
 
 resource "azurerm_service_plan" "plan" {
-  location = azurerm_resource_group.rgasp.location
-  name = var.service_plan_name
-  os_type = var.os
+  location            = azurerm_resource_group.rgasp.location
+  name                = var.service_plan_name
+  os_type             = "Linux"  
   resource_group_name = azurerm_resource_group.rgasp.name
-  sku_name = var.pricing_plan
+  sku_name            = var.pricing_plan
 }
 
-resource "azurerm_windows_web_app" "serviceApp" {
-  service_plan_id = azurerm_service_plan.plan.id
-  location = azurerm_service_plan.plan.location
-  name = var.linux_web_app_name
+resource "azurerm_linux_web_app" "serviceApp" {
+  name                = var.linux_web_app_name
+  location            = azurerm_service_plan.plan.location
   resource_group_name = azurerm_resource_group.rgasp.name
+  service_plan_id     = azurerm_service_plan.plan.id
+
   site_config {
     application_stack {
-      dotnet_version = "v8.0"
+      node_version = "22-lts" 
     }
-}
-    
-}
+  }
 
+  https_only = true
+}
