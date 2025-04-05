55091844a3d7ef2d79c664c24e4d8006700d8156
Sakshi Mathur <147843432+sakshimathur03@users.noreply.github.com>
2025-04-05 14:32:45 +0530

    Create variables.tf

---
+++ Terraform/variables.tf
@@ -0,0 +1,47 @@
+
+
+variable "location" {
+    description = "location of service"
+    type = string
+    default = "centralus"
+}
+variable "resource_group_name" {
+  description = "resource group name"
+  type = string
+  default = "rg-jenkins-react"
+}
+
+variable "os" {
+  description = "Operating system"
+  type = string
+  default = "Linux"
+}
+
+variable "service_plan_name" {
+  description = "Name of the service plan"
+  type = string
+  default = "myapp-plan"
+}
+
+variable "sku_name" {
+  description = "Pricing plan of the azure service plan"
+  type = string
+  default = "Free"
+}
+
+variable "linux_web_app_name" {
+  description = "name of the app service"
+  type = string
+  default = "webapijenkins2543"
+}
+variable "pricing_plan" {
+  description = "Pricing plan of the azure service plan"
+  type = string
+  default = "F1"
+}
+
+variable "linux_web_app_slot_name" {
+  description = "name of windows web app slot"
+  type = string
+  default = "winappslotSakshi"
+}
